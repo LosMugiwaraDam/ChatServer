904a13e9282cec0dc5f18cb832f92aaefa21a6fe
Jie Zhang <jiemartinez12@gmail.com>
2023-02-14 19:30:34 +0100

    no message

--- src/main/java/Chat/Server/App.java
+++ src/main/java/Chat/Server/App.java
@@ -3,10 +3,6 @@ package Chat.Server;
 import clientService.ClientService;
 import util.UtilService;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
